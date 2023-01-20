6ce9d1f2e3a5321ee5e6a81357305a4d6300bacf
Vishaka Yadav <vyadav@ncsu.edu>
2023-01-19 19:43:37 -0500

    [feat]: package HW1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='HW1',
+  version='1.0.0',
+  packages=['HW1', 'HW1.src', 'HW1.test'],
+  license='LICENSE.md',
+  description='Sample project with HELP text and Test Suite',
+  long_description=open('README.md').read(),
+)
