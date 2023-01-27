20f9e591bdc4797d77042ffe1ba4e211bee04fab
Vishaka Yadav <vyadav@ncsu.edu>
2023-01-26 20:35:46 -0500

    [release]: 2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='ASE_Assignments',
-  version='1.0.0',
+  version='2.0.0',
   packages=['HW1', 'HW1.src', 'HW1.test',
             'HW2', 'HW2.src', 'HW2.test'],
   license='LICENSE.md',
